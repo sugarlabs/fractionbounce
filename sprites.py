sugarlabs/fractionbounce

commit 4af7683120345f25e78582886cb4d0162317ec68

Fix Python GI API version warnings

sprites.py
@@ -77,6 +77,7 @@ def svg_str_to_pixbuf(svg_string):
 '''
 
 import gi
+gi.require_version('PangiCairo', '1.0')
 from gi.repository import Gtk, GdkPixbuf, Gdk
 from gi.repository import Pango, PangoCairo
 import cairo

FractionBounceActivity.py
@@ -15,6 +15,8 @@
 
 import os
 
+import gi
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from gi.repository import Gdk
 

commit 09edbc61922ec51c92f48ab39851e51fe6ca254e

Fix Python GI API version warnings, again
Regression in 4af7683, traceback, namespace not available.  Also a
testing and usage canary, master was unusable for over a year.

sprites.py
@@ -77,7 +77,7 @@ def svg_str_to_pixbuf(svg_string):
 '''
 
 import gi
-gi.require_version('PangiCairo', '1.0')
+gi.require_version('PangoCairo', '1.0')
 from gi.repository import Gtk, GdkPixbuf, Gdk
 from gi.repository import Pango, PangoCairo
 import cairo